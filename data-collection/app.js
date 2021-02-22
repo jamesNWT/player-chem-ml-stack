7a73c09d0cac7f0d8bcfd54ae5bd6f5bed2289a4
James <jameswilcox.98@gmail.com>
2021-02-22 18:30:02 -0400

    played around with the API and added a 'dev journal' to keep track of thoughts and for posterity or something

--- data-collection/app.js
+++ data-collection/app.js
@@ -12,9 +12,18 @@ async function main() {
     my_filter = {startDate: '2017-07-10', 
                  endDate: '2017-07-18', 
                  rankingFilter: 'Top20'};
+    
+    my_match = {id: 2306295}    
 
-    my_data = await HLTV.getMatchesStats(my_filter);
+    // my_data = await HLTV.getMatchesStats(my_filter);
+    // console.log(my_data[0])
 
-    console.log(my_data[0])
+    // my_data = await HLTV.getMatch(my_match);
+    // console.log(my_data.players.team1[0])
 
+    my_data = await HLTV.getMatchMapStats({id: 29968});
+    console.log(my_data.playerStats.team1[0].rating)
+
+    my_player = await HLTV.getPlayer({id: 798});
+    // console.log(my_player)
 }
---
+++ dev-journal.md
@@ -0,0 +1,6 @@
+### 2021-02-22
+
+Was about to give in to despair with the API not returning nearly enough info 
+with the GetMatchesStats function, but found that getMatchMapStats function will
+the info I need. So, I'll get a list of match ID's using the former and then get
+useful information out of these matches with the former.
